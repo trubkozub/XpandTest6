79ed5831982b17f8d77ddd7ee5c59daf7be0eb80
trubkozub <trubkozub@DESKTOP-D8F9MN8>
2021-03-31 09:15:04 +0300

    Add Controller Accounts

---
+++ XpandTest6/Controllers/AccountsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using XpandTest6.Models;
+
+namespace XpandTest6.Controllers
+{
+    public class AccountsController : Controller
+    {
+
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AccountsController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
