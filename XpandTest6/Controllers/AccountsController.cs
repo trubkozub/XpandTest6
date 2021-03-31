4be78b5c06f388cfa3fd80ece49fff8b353327a4
trubkozub <trubkozub@DESKTOP-D8F9MN8>
2021-03-31 11:12:39 +0300

    Add action Login in Accounts

--- XpandTest6/Controllers/AccountsController.cs
+++ XpandTest6/Controllers/AccountsController.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using XpandTest6.Models;
+using XpandTest6.ViewModels;
 
 namespace XpandTest6.Controllers
 {
@@ -24,6 +25,43 @@ namespace XpandTest6.Controllers
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl = null)
+        {
+            return View(new LoginViewModel { ReturnUrl = returnUrl });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByNameAsync(model.Email);
+                if (user != null)
+                {
+                    // проверяем, подтвержден ли email
+                    if (!await _userManager.IsEmailConfirmedAsync(user))
+                    {
+                        ModelState.AddModelError(string.Empty, "Вы не подтвердили свой email");
+                        return View(model);
+                    }
+                }
+
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, false, false);
+                if (result.Succeeded)
+                {
+
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Неправильный логин и (или) пароль");
+                }
+            }
+            return View(model);
         }
     }
 }
