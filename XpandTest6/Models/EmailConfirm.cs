c065084d115e8e124fa1bcad45d901261d395305
trubkozub <trubkozub@DESKTOP-D8F9MN8>
2021-03-31 13:22:02 +0300

    ok

---
+++ XpandTest6/Models/EmailConfirm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XpandTest6.Models
+{
+    public class EmailConfirm
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+    }
+}
---
+++ XpandTest6/Models/ResetPasswordViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XpandTest6.Models
+{
+    public class ResetPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Password must contain at least 6 characters", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "Password mismatch")]
+        public string ConfirmPassword { get; set; }
+
+        public string Code { get; set; }
+    }
+}
